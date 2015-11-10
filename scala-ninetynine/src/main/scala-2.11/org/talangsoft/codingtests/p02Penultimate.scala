1015d26b5fb7915df94b172a0fb2465b3ab27adc
Tamas Lang <tamas.lang@talangsoft.org>
2015-11-10 14:07:24 +0000

    p02 added @tailrec

--- scala-ninetynine/src/main/scala-2.11/org/talangsoft/codingtests/p02Penultimate.scala
+++ scala-ninetynine/src/main/scala-2.11/org/talangsoft/codingtests/p02Penultimate.scala
@@ -1,6 +1,10 @@
 package org.talangsoft.codingtests
 
+import scala.annotation.tailrec
+
 object p02Penultimate {
+  
+  @tailrec
   def penultimate[T](l: List[T]):T=  {
     if(l.size < 2) throw new NoSuchElementException
     else if(l.size == 2) l.head
